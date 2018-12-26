812e0413c9ca5c2a00d486b665d8e8aec7d06a9e
utkarsh867 <utkarsh867@gmail.com>
2018-12-26 21:41:47 +0530

    Added code for handling realtime database

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,24 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const firebase = require('firebase');
+const CONFIG = require('./CONFIG.json');
+const serviceAccount = require('./serviceKey.json');
 
 exports.hello = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    if(!admin.apps.length){
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: "https://rcsls-2018.firebaseio.com"
+        }); 
+    }
+
+    admin.database().ref('washers/').set({
+        washer1 : 2
+    });
+    
+    response.send("Hello from Firebase!");
 });
+
+exports.bye = functions.https.onRequest((request,  response) => {
+    response.send("Good Bye")
+})
